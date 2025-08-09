bdcafac3121a47fdfc8904014e2287abb4535a27
gfbatista <gilbertoferrari@gmail.com>
2025-08-09 10:47:37 -0300

    feat: add sidebar itens

--- src/components/Sidebar/index.jsx
+++ src/components/Sidebar/index.jsx
@@ -20,6 +20,30 @@ const Sidebar = () => {
           >
             Início
           </ItemNav>
+          <ItemNav
+            activeIcon="/icons/mais-vistas-ativo.png"
+            inactiveIcon="/icons/mais-vistas-inativo.png"
+          >
+            Mais vistas
+          </ItemNav>
+          <ItemNav
+            activeIcon="/icons/mais-curtidas-ativo.png"
+            inactiveIcon="/icons/mais-curtidas-inativo.png"
+          >
+            Mais curtidas
+          </ItemNav>
+          <ItemNav
+            activeIcon="/icons/novas-ativo.png"
+            inactiveIcon="/icons/novas-inativo.png"
+          >
+            Novas
+          </ItemNav>
+          <ItemNav
+            activeIcon="/icons/surpreenda-me-ativo.png"
+            inactiveIcon="/icons/surpreenda-me-inativo.png"
+          >
+            Surpreenda-me
+          </ItemNav>
         </StyledList>
       </nav>
     </aside>
